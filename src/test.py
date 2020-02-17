6ea845e21fcf4b28433f004f9a464b6c89ec2eaf
Pere <pere.solaclaver@gmail.com>
2020-02-17 16:11:33 +0000

    Store cumulative count

--- src/test.py
+++ src/test.py
@@ -5,8 +5,13 @@ def count_sort(arr):
     max_value = max(arr)
     len_count_list = max_value + 1
     count_list = [0] * len_count_list
+    cum_count = 0
     for i in range(0, len(arr)):
         count_list[arr[i]] += 1
+    for i in range(0, len(count_list)):
+        cum_count += count_list[i]
+        count_list[i] = cum_count
+
     return count_list
 
 
