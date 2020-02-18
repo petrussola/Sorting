6ddc2c276eeee5828a1a1b153d927b4bc3fdcc40
Pere <pere.solaclaver@gmail.com>
2020-02-18 22:07:07 +0000

    Commiting other files not in same folder as previous algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,20 +2,20 @@
 def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        # cur_index = i
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        for j in range(cur_index + 1, len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
 
         # TO-DO: swap
         temp_low = arr[smallest_index]
-        arr[smallest_index] = arr[cur_index]
-        arr[cur_index] = temp_low
-        cur_index += 1
-        smallest_index = cur_index
+        arr[smallest_index] = arr[i]
+        arr[i] = temp_low
+        # cur_index += 1
+        smallest_index = i
     return arr
 
 # TO-DO:  implement the Bubble Sort function below
--- src/test3.py
+++ src/test3.py
@@ -1,13 +1,45 @@
 test = [1, 3, 2]
 test2 = [4, 7, 5]
+test3 = [7, 3, 1, 6, 9, 4, 5, 2, 8]
+test4 = [12, 21, 34, 21, 45, 67, 54, 38, 9, 5, 2, 17, 28]
 
 
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    merged_arr = arrA + arrB
+    # check first items in arrays and place the smallest one as position of new array
+    for k in range(elements):
+        if len(arrA) == 0:
+            merged_arr[k] = arrB[0]
+            del arrB[0]
+        elif len(arrB) == 0:
+            merged_arr[k] = arrA[0]
+            del arrA[0]
+        else:
+            if arrA[0] < arrB[0]:
+                merged_arr[k] = arrA[0]
+                del arrA[0]
+            else:
+                merged_arr[k] = arrB[0]
+                del arrB[0]
+    # if one array is empty and the other one has one item left, add this item to the array without further check
+
     return merged_arr
 
 
-print(merge(test, test2))
+# print(merge(test, test2))
+
+def merge_sort(arr):
+    # TO-DO
+    if len(arr) <= 1:
+        return arr
+    middle = len(arr) // 2
+    lhs = arr[:middle]
+    rhs = arr[middle:]
+    left = merge_sort(lhs)
+    right = merge_sort(rhs)
+    return merge(left, right)
+
+
+print(merge_sort(test4))
